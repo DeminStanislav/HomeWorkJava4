870c1e2937360347c57688314171d67c573b264c
Stanislav <deminstanislav1988@mail.ru>
2023-05-02 18:29:47 +0500

    New commit

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
         int amount = 100;
         int fill = 2000;
         int bonus;
-        if (fill <1000) {
+        if (fill < 1000) {
             bonus = 0;
         } else {
             bonus = fill / 100 * 1;
